53fddd3041e22d22b12c6b3f154a13ee8528d759
Zbigniew Kuziemski <z.kuziemski00@gmail.com>
2023-09-04 10:32:59 +0200

    Add project files.

---
+++ DatingAppp/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using DatingAppp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> users { get; set; }
+    }
+}
